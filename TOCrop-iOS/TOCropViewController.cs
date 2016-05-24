0647f46a9049fba75d8aead597508cbde066102c
Erwan Jezequel <erwan.jezequel@apcurium.com>
2016-05-24 16:13:43 -0400

    TOCrop Added comments

--- TOCrop-iOS/TOCropViewController.cs
+++ TOCrop-iOS/TOCropViewController.cs
@@ -7,12 +7,13 @@ namespace TOCrop
     public partial class TOCropViewController
     {
         /// <summary>
-        /// Shows the crop view async.
+        /// Shows the crop view and wait for the user's input.
         /// </summary>
-        /// <returns>The crop view async.</returns>
+        /// <returns>Returns a byte array of the cropped photo or throws an OperationCancelledException if the user cancelled.</returns>
         /// <param name="rootViewController">Root view controller.</param>
         /// <param name="isAnimated">If set to <c>true</c> is animated.</param>
         /// <param name="completionHandler">Completion handler.</param>
+        /// <exception cref="System.OperationCanceledException">Thrown when the user hits cancel</exception>
         public Task<byte[]> ShowCropViewAsync(UIViewController rootViewController, bool isAnimated, Action completionHandler)
         {
             var cropViewDelegate = new CropViewControllerDelegate();
