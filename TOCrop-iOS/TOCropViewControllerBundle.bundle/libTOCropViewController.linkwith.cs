4a51acc10584944887e584aad67e5b495be18eaa
Dominique Savoie <dominique.savoie@apcurium.com>
2016-06-29 15:11:53 -0400

    Added ObjC flag

--- TOCrop-iOS/TOCropViewControllerBundle.bundle/libTOCropViewController.linkwith.cs
+++ TOCrop-iOS/TOCropViewControllerBundle.bundle/libTOCropViewController.linkwith.cs
@@ -1,3 +1,9 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libTOCropViewController.a", SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith (
+    "libTOCropViewController.a",
+    LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.ArmV7, 
+    "-ObjC",
+    SmartLink = true, 
+    ForceLoad = true, 
+    IsCxx = true)]
